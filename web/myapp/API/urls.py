37c85e218c7e3925bff34f0b1f0921ae43b4729c
Lewis Joo Hyeong Lee <jhhome3190@naver.com>
2023-07-10 19:46:01 +0000

    FEAT: save user's bookmark data with UserInfo(dependent)

--- web/myapp/API/urls.py
+++ web/myapp/API/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
+from django.contrib import admin
 
 from . import views
 
 urlpatterns = [
+    # path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('test_request/', views.test_request, name='test_request'),
     path('test_response/', views.test_response, name='test_response'),
     path('inference/', views.inference, name='inference'),
+    path('post/', views.post_api, name='post'),
+    path('get/', views.get_api, name='post')
 ]
