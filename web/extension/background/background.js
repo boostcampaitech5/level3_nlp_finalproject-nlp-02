489ac4aea40728e737693db2c823041f8bddac07
Lewis JooHyeong Lee <jhhome3190@naver.com>
2023-07-12 21:38:28 +0900

    FEAT: #9 send user's new bookmark data to server

--- web/extension/background/background.js
+++ web/extension/background/background.js
@@ -1,38 +1,37 @@
+const SERVER_URL = 'http://118.67.131.212:30005/API/post/'
+
 // bookmark listener
 chrome.bookmarks.onCreated.addListener(function (bookmark) {
   chrome.bookmarks.get(bookmark, function (bookmarks) {
     if (bookmarks && bookmarks.length > 0 && bookmarks[0].url) {
       const bookmarkedUrl = bookmarks[0].url
-      console.log("new bookmark added. url from background: ", bookmarkedUrl)
+      // console.log("New bookmark added. url from background: ", bookmarkedUrl)
      }
   });
 });
 
-// message listener
+// message listener  
+// send bookmark information to server
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   let pageInfo = request.pageInfo;
 
-  chrome.identity.getProfileUserInfo({'accountStatus':'ANY'}, function(userInfo) { 
-    console.log(userInfo)
-    console.log(typeof userInfo)
+  chrome.identity.getProfileUserInfo({'accountStatus':'ANY'}, function(userProfile) { 
+    const userName = userProfile.email.match(/^([^@]*)@/)[1];
 
     // Merge pageInfo and userInfo into totalInfo
+    userInfo = {'userId': userName, 'userEmail': userProfile['email']}
     const totalInfo = Object.assign(pageInfo, userInfo);
     console.log(totalInfo)
     console.log(typeof totalInfo)
 
-    // Send data to Server
-    fetch('http://101.101.211.226:30006/API/inference', {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(totalInfo)
-    })
-    .then(response => console.log(response.json()))
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+    // 북마크 정보 전송
+    simpleFetcher(SERVER_URL, totalInfo)
+      .then(responseData => {
+        if (responseData) {
+          console.log("Sending bookmark is done!, ", responseData);
+        }
+      })
+
   });
 });
 
@@ -53,3 +52,25 @@ chrome.runtime.onConnect.addListener(function(port) {
         console.log("onConnect does not work")
     }
 });
+
+////////////////
+// 심플한 fetch 함수 구현
+function simpleFetcher(url, data){
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Network response is not ok')
+      }
+    })
+    .catch(error =>{
+      console.error('Error:', error);
+    })
+}
