f0ed8a13032d383cc19bf75f2ecc086e62248fca
soyoung <soyljhg@gmail.com>
2023-07-12 09:07:08 +0000

    FIX: prompt 수정 및 csv에서 바로 test가능하도록 수정

--- data/data_gen.py
+++ data/data_gen.py
@@ -1,6 +1,7 @@
 import os
 import warnings
 import openai
+import pandas as pd
 
 from tqdm import tqdm
 
@@ -8,13 +9,21 @@ warnings.filterwarnings(action='ignore')
 
 # 설정해 줄 것
 # api_key
-# text
+# csv 경로
+# index
 openai.api_key = ''
-text = ''' '''
+df=pd.read_csv('/opt/ml/level3_nlp_finalproject-nlp-02/data/datasets/Tistory_summarize.csv')
+index = 10
+text = df.iloc[10]['summarize']
+title = '\n≈'+df.iloc[10]['title']+'≈\n'
 
 model_name = 'text-davinci-003'
 
-prompt_user = ''' Q: ᴥ{}ᴥ 사이에 들어갈 문서에 어울리는 5개의 태그를 알려줘. 추가 정보1: 태그 형식은 #영어(한글). \n 추가 정보2: 한글로만 이루어진 문서라도 #english(한글)이렇게 작성해 줘. \n\n ᴥ''' + text + " ᴥ \n A: "
+prompt_user = '''Q: ᴥ{}ᴥ 사이에 들어갈 문서에 어울리는 5개의 태그를 알려줘. 
+추가 정보1: 태그 형식은 #영어(한글). \n 
+추가 정보2: 한글로만 이루어진 문서라도 #english(한글)이렇게 작성해 줘.\n 
+추가 정보3: ≈{}≈사이에 들어가는건 문서의 제목이니 가중치를 높여 줘. \n
+ᴥ''' + title+ text + " ᴥ \n A: "
 
 response = openai.Completion.create(
     model = model_name,
@@ -26,4 +35,5 @@ response = openai.Completion.create(
     presence_penalty = 0.0,
     stop =["\n"]
 )
+print(prompt_user)
 print(response.choices[0].text.strip())
