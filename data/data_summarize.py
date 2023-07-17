3c1660523cc27884bcbc80669138a0e3848aaf4c
soyoung <soyljhg@gmail.com>
2023-07-17 19:37:21 +0000

    FIX: #19 블로그 한 줄 요약 프로세스 완성

--- data/data_summarize.py
+++ data/data_summarize.py
@@ -2,14 +2,16 @@ import pandas as pd
 import torch
 import nltk
 import gc
+import os
 
 from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 nltk.download('punkt')
+root_path = '/opt/ml/level3_nlp_finalproject-nlp-02/data/datasets/'
 
 df=pd.read_csv('/opt/ml/level3_nlp_finalproject-nlp-02/data/datasets/0706_Tistory_Context.csv')
-
+print('230714_Tistory summarization')
 print('model and tokenizer build...')
 model_dir = "lcw99/t5-large-korean-text-summary"
 model = AutoModelForSeq2SeqLM.from_pretrained(model_dir)
@@ -51,7 +53,7 @@ for i in range(3,len(df)//n):
   print(len(df_tmp))
   df_tmp['summarize'] = df_tmp['context'].progress_apply(lambda x: summarize(x))
   print('making csv...')
-  df_tmp.to_csv('/opt/ml/level3_nlp_finalproject-nlp-02/data/datasets/Tistory/Tistory_summarize_'+str(i*n).zfill(5)+'.csv', sep=',', na_rep='NaN',index=False) # do not write index
+  df_tmp.to_csv('/opt/ml/level3_nlp_finalproject-nlp-02/data/datasets/dataset/Tistory/Tistory_summarize_'+str(i*n).zfill(5)+'.csv', sep=',', na_rep='NaN',index=False) # do not write index
   print('DONE!')
 
 print('---------------------'+str((i+1)*n).zfill(5),str((i+1)*n+(len(df)%n)).zfill(5)+'---------------------')
@@ -60,5 +62,17 @@ tqdm.pandas()
 print(len(df_tmp))
 df_tmp['summarize'] = df_tmp['context'].progress_apply(lambda x: summarize(x))
 print('making csv...')
-df_tmp.to_csv('/opt/ml/level3_nlp_finalproject-nlp-02/data/datasets/Tistory/Tistory_summarize_'+str((i+1)*n).zfill(5)+'.csv', sep=',', na_rep='NaN',index=False) # do not write index
+df_tmp.to_csv('/opt/ml/level3_nlp_finalproject-nlp-02/data/datasets/dataset/Tistory/Tistory_summarize_'+str((i+1)*n).zfill(5)+'.csv', sep=',', na_rep='NaN',index=False) # do not write index
+print('DONE!')
+
+print('concat csv...')
+list_ = os.listdir('/opt/ml/level3_nlp_finalproject-nlp-02/data/datasets/dataset/Tistory/')
+df = pd.DataFrame()
+path = '/opt/ml/level3_nlp_finalproject-nlp-02/data/datasets/dataset/Tistory/'
+for n in sorted([n for n in list_ if 'summarize' in n]):
+    df_tmp=pd.read_csv(path+n,  lineterminator='\n')
+    df = pd.concat([df,df_tmp])
+print(f"total dataset length: {len(df)}")
+print(f"null dataset length: {len(df[df['tag'].isnull()])}")
+df.to_csv('/opt/ml/level3_nlp_finalproject-nlp-02/data/datasets/Velog_summarize.csv', sep=',', na_rep='NaN',index=False) # do not write index
 print('DONE!')
