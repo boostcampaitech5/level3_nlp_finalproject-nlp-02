652bbd262ef35b6576b94929ee77023378ea6938
Kim, Gi-Beom <gibum1228@gmail.com>
2023-07-18 01:53:13 +0900

    FEAT: ERD 확정된 거 models.py에 구현

--- web/myapp/API/models.py
+++ web/myapp/API/models.py
@@ -27,3 +27,46 @@ class Bookmarks(models.Model):
         
     def __str__(self):
         return str(self.userId)
+    
+# 유저 정보
+class Customer(models.Model):
+    '''
+    업데이트 예정
+    '''
+    id          = models.CharField(max_length=50, primary_key=True)
+    email       = models.EmailField(max_length=50, unique=True)
+    pwd         = models.CharField(max_length=50)
+    name        = models.CharField(max_length=10)
+    birth       = models.DateField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+# 북마크 정보
+class Bookmark(models.Model):
+    '''
+    업데이트 예정
+    '''
+    no          = models.AutoField(primary_key=True)
+    url         = models.URLField(max_length=300)
+    title       = models.CharField(max_length=50)
+    tags        = models.TextField()
+    content     = models.TextField()
+    summarize   = models.TextField(blank=True, null=True)
+    reference   = models.CharField(max_length=50)
+    topic       = models.CharField(max_length=50, blank=True, null=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+# 유저의 북마크 저장 정보
+class Bookmark_Of_Customer(models.Model):
+    '''
+    업데이트 예정
+    '''
+    customer_id         = models.CharField(max_length=50, primary_key=True)
+    bookmark_no         = models.AutoField(primary_key=True)
+    tags                = models.TextField()
+    name                = models.CharField(max_length=50)
+    create_date         = models.DateTimeField(auto_now_add=True)
+    update_date         = models.DateTimeField(auto_now=True)
+    save_path_at_chrome = models.CharField(max_length=100)
+    save_path_at_ours   = models.CharField(max_length=100)
