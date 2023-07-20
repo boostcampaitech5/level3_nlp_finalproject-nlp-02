e6870b62baae0ca94b2e655fa81c3ea39a7fc688
Lewis Joo Hyeong Lee <jhhome3190@naver.com>
2023-07-20 08:42:59 +0000

    FEAT: #22 DB saving sequence update

--- web/myapp/API/views.py
+++ web/myapp/API/views.py
@@ -4,19 +4,20 @@ import json
 import logging
 
 from django.shortcuts import render
+from django.utils import timezone
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import UserInfo, Bookmarks
+from .models import UserInfo, Bookmarks, Bookmark, Bookmark_Of_Customer
 from .utils import *
-from .serializers import PostSerializer
+# from .serializers import PostSerializer
 
 # dl_model_path = '../../model/models/'
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'model'))
-from models.tagging_model import get_tag_from_model
+# sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'model'))
+# from models.tagging_model import get_tag_from_model
 
 logger = logging.getLogger(__name__)
 
@@ -56,43 +57,26 @@ def inference(request):
             return JsonResponse({'success': False, 'error': 'Invalid data'})
 
 
+# 단일 user bookmark 정보 송신 및 DB 저장
 @api_view(['POST'])
 def post_api(request):
+    """
+    Note:
+        1. 기존 북마크 table search
+        2. 없으면 북마크 table에 추가
+        3. Bookmark_of_user 에 id 추가, bookmark no는 기존에 있었다면 search 한 결과, 없었다면 추가한 no.
+        4. 중복 저장 시퀀스 예외처리
+    """
     if request.method == 'POST':
-        
         data = json.loads(request.body.decode('utf-8'))
         # logger.info("\ndata: %s, %s", data, type(data))
         
-        try:
-            data['userNumber'] = UserInfo.objects.get(userId=data['userId'])
-
-        except ObjectDoesNotExist:
-            logger.info("[ERROR] Object Does Not Exist. DB will save userInfo first...")
-            
-            # 새로운 유저 정보 업데이트
-            newUser = {'userId': data['userId'],
-                       'userEmail': data['userEmail'],
-                       'userPassword': '',
-                       }
-            UserInfo.objects.create(**newUser)
-            
-            logger.info("DB updates new userInfo.")
+        flag = save_single_bookmark(data)
         
-        # 기타 오류
-        except Exception as e:
-            logger.exception("[Exception] Unexpected error occurred while saving the bookmark.")
+        if flag:
+            return JsonResponse({'success': True})
+        else:
             return Response({'success': False}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        
-        # Foreign key 재 업데이트
-        data['userNumber'] = UserInfo.objects.get(userId=data['userId'])
-        
-        # 딥러닝 모델로부터 태그 정보 inference
-        # tags = get_tag_from_model(data['content'])
-        # data['tag'] = tags  # sample tag for now
-        
-        # 북마크 정보 저장
-        Bookmarks.objects.create(**data)
-        return JsonResponse({'success': True})
         
     else:
         logger.error("[ERROR] Illegal request is requested.")
@@ -102,37 +86,23 @@ def post_api(request):
 # user bookmark history 정보 송신 및 DB 저장
 @api_view(['POST'])
 def post_history(request):
+    """
+    Note:
+        1. 리스트로 전달되는 북마크 히스토리 중 북마크 DB에 저장되지 않은 url 탐색 후 저장
+        2. 해당 유저의 Bookmark_Of_Customer 업데이트
+        3. 마찬가지로 중복 북마크 예외처리
+    """
     if request.method == 'POST':
-        
         data = json.loads(request.body.decode('utf-8'))
-        print("post_history requested!")
+        flag = []
+        # logger.info("post_history requested!")
         
-        try:
-            data[0]['userNumber'] = UserInfo.objects.get(userId=data[0]['userId'])  # bulk 이니까, 첫 데이터로 try.
+        flag = [save_single_bookmark(single_data) for single_data in data if single_data]
 
-        except ObjectDoesNotExist:
-            logger.info("[ERROR] Object Does Not Exist. DB will save userInfo first...")
-            
-            # 새로운 유저 정보 업데이트
-            newUser = {'userId': data[0]['userId'],
-                       'userEmail': data[0]['userEmail'],
-                       'userPassword': '',
-                       }
-            UserInfo.objects.create(**newUser)
-        
-        # bulk 처리.
-        for bookmark_data in data:
-            try:
-                bookmark_data['userNumber'] = UserInfo.objects.get(userId=bookmark_data['userId'])
-            except:
-                pass    # undefined 인 경우는 pass.
-        logger.info("DB updates new userInfo.")
-        
-        # 북마크 정보 저장
-        bookmark_objects = [Bookmarks(**bookmark_data) for bookmark_data in data if bookmark_data]
-        Bookmarks.objects.bulk_create(bookmark_objects)
-        
-        return JsonResponse({'success': True})
+        if all(flag):
+            return JsonResponse({'success': True})
+        else:
+            return Response({'success': False}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     else:
         logger.error("[ERROR] Illegal request is requested.")
@@ -146,4 +116,56 @@ def get_api(request):
     logger.info("answer: ", bookmarked)
     
     return JsonResponse({'sucess': True, 'data': 'its from server'})
-    # return HttpResponse("get 호출됨")
+    # return HttpResponse("get 호출됨")    
+    
+    
+def save_single_bookmark(data):
+    # 기존 북마크 table search
+    url_result = Bookmark.objects.filter(url=data['url'])
+
+    try:
+        url_no = url_result[0].no
+        
+    except (AttributeError, IndexError) as e:
+        # 없으면 북마크 table에 추가
+        new_bookmark_info = {
+            'url': data['url'],
+            'title': data['title'],
+            'content': data['content'],
+            'summarize': "",
+            'reference': "",
+            'topic': "",
+            'tags': "",
+            # 'create_date': ,
+            # 'update_date': ,
+        }
+        
+        Bookmark.objects.create(**new_bookmark_info)
+        url_no = Bookmark.objects.filter(url=data['url'])[0].no
+    
+    # 기타 오류
+    except Exception as e:
+        logger.exception("[Exception] Unexpected error occurred while saving the bookmark.")
+        return False
+
+    # 중복 저장 예외 시퀀스 추가
+    dup_bookmark = Bookmark_Of_Customer.objects.filter(customer_id=data['customer_id']).filter(bookmark_no=url_no)
+    
+    if dup_bookmark:
+        logger.exception("[Exception] User already saved same url.")
+        return True
+    
+    # Bookmark_of_user에 id 추가
+    new_bookmark_of_customer_info = {
+        'customer_id': data['customer_id'],
+        'bookmark_no': url_no,
+        'tags': "",
+        'name': "",
+        # 'create_date': "",
+        # 'update_date': "",
+        'save_path_at_chrome': "",
+        'save_path_at_ours': "" ,
+    }
+    
+    Bookmark_Of_Customer.objects.create(**new_bookmark_of_customer_info)
+    return True
